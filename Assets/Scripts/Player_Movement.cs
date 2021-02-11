7af83e0f19e543456c0a2be788d073331354d8f7
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-02-12 06:06:13 +0700

    Jump increased in height when hold down jump
    Tap jump = short jump
    Hold jump = high jump

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,24 +7,65 @@ public class Player_Movement : MonoBehaviour
     public float MovementSpeed = 1;
     public float Jumpforce = 1;
 
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float JumpTimeCounter;
+    public float JumpTime;
+    private bool isJumping;
+
     private Rigidbody2D _rigidbody;
 
-    // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //Movement Left and right
         var movement = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+    }
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && Input.GetButtonDown("Jump")) 
         {
-            _rigidbody.AddForce(new Vector2(0, Jumpforce), ForceMode2D.Impulse);
+            isJumping = true;
+            JumpTimeCounter = JumpTime;
+            _rigidbody.velocity = Vector2.up * Jumpforce;
         }
+
+        if (Input.GetButton("Jump") && isJumping == true)
+        {
+            if(JumpTimeCounter > 0)
+            {
+                _rigidbody.velocity = Vector2.up * Jumpforce;
+                JumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+
+        //Movement Jumping
+        //if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        //{
+        //    _rigidbody.AddForce(new Vector2(0, Jumpforce), ForceMode2D.Impulse);
+        //}
+
+
     }
 
 }
