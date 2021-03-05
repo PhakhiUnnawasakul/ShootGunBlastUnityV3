ef53c4900cbd6b0a5707877e7580c789bfb45c87
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-03-05 18:57:15 +0700

    Change enemy AI structure

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public float Range;
-    public Transform Target;
-    bool Detected = false;
-
-    Vector2 Direction;
-    public GameObject Gun;
-    public GameObject Bullet;
 
     public int health;
-    
-    public float FireRate;
-    float nextShoot = 0;
-    public Transform EnemyGunpoint;
 
-    public float BulletForce;
 
     void Update()
     {
@@ -27,56 +15,8 @@ public class EnemyAI : MonoBehaviour
             Destroy(gameObject);
         }
 
-
-        //Direction of where the player is
-        Vector2 targetPos = Target.position;
-        Direction = targetPos - (Vector2)transform.position;
-
-        //Raycasting to find player
-        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, Direction, Range);
-
-        if (rayInfo)
-        {
-            //if detected object with tag player, do something
-            if(rayInfo.collider.gameObject.tag == "Player")
-            {
-                //if false, turn detection to true.s
-                if(Detected == false)
-                {
-                    Detected = true;
-                }
-            }
-            else
-            {
-                if(Detected == true)
-                {
-                    Detected = false;
-                }
-            }
-
-        }
-
-        //if player is detected, do something
-        if (Detected)
-        {
-            //make gun turn
-            Gun.transform.right = Direction;
-
-            if(Time.time > nextShoot)
-            {
-                nextShoot = Time.time + 1 / FireRate;
-                shoot();
-            }
-        }
-
     }
 
-
-    void shoot()
-    {
-        GameObject BulletIns = Instantiate(Bullet, EnemyGunpoint.position, Quaternion.identity);
-        BulletIns.GetComponent<Rigidbody2D>().AddForce(Direction * BulletForce);
-    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -85,14 +25,6 @@ public class EnemyAI : MonoBehaviour
             Destroy(col.gameObject);
             Destroy(gameObject);
         } 
-    }
-
-
-    //Draw sphere of the detection range
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawWireSphere(transform.position, Range);
-        
     }
 
     public void TakeDamage(int damage)
