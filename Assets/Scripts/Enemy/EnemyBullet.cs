175e5f3c099c2f2b393489b69ad3b12c8a2f00df
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-03-05 22:15:08 +0700

    Damage system with player and Enemy

--- Assets/Scripts/Enemy/EnemyBullet.cs
+++ Assets/Scripts/Enemy/EnemyBullet.cs
@@ -6,6 +6,8 @@ public class EnemyBullet : MonoBehaviour
 {
     private Rigidbody2D _rb;
     public float speed = 2f;
+    [SerializeField] private float lifeTime = 10f;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -14,13 +16,25 @@ public class EnemyBullet : MonoBehaviour
     void Update()
     {
         _rb.MovePosition(transform.TransformPoint(-Vector3.right * speed * Time.deltaTime));
-        
+        lifeTime -= Time.deltaTime;
+        if(lifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-
-
-    void OnCollisionEnter2D(Collision2D Col)    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        PlayerHP targetHealth = collision.GetComponent<PlayerHP>();
+        if(targetHealth != null)
+        {
+            targetHealth.TakeDamage(10);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)    
+    {
+
         Destroy(this.gameObject);
         
     }
