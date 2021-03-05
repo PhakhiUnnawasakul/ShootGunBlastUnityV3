a40beb80d42ff1d9eabeed98b07ae57b30ffc0c1
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-03-05 21:34:29 +0700

    New enemyAI can now shoot

--- Assets/Scripts/Enemy/EnemyBullet.cs
+++ Assets/Scripts/Enemy/EnemyBullet.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    public float speed = 2f;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _rb.MovePosition(transform.TransformPoint(-Vector3.right * speed * Time.deltaTime));
+        
+    }
+
+
+
     void OnCollisionEnter2D(Collision2D Col)    
     {
         Destroy(this.gameObject);
--- Assets/Scripts/Enemy/EnemyTurret.cs
+++ Assets/Scripts/Enemy/EnemyTurret.cs
@@ -6,7 +6,15 @@ public class EnemyTurret : MonoBehaviour
 {
     [SerializeField] private float scanRadius = 3f;
     [SerializeField] private LayerMask layers;
+    [SerializeField] private GameObject Bullet;
+    [SerializeField] private Transform Gunpoint;
+    [SerializeField] private float ShotDelay = 1f;
     private Collider2D target;
+
+    private void Start()
+    {
+        InvokeRepeating("Shoot", 0f, ShotDelay);
+    }
 
     private void Update()
     {
@@ -27,6 +35,14 @@ public class EnemyTurret : MonoBehaviour
             Vector2 direction = target.transform.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180f;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    void Shoot()
+    {
+        if(target != null)
+        {
+            Instantiate(Bullet, Gunpoint.position, Gunpoint.rotation);
         }
     }
 
