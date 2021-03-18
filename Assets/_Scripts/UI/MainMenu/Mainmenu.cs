9b49aec31dd51b5c6f166d9da114a2379ff064b0
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-03-18 15:19:43 +0700

    Mainmenu Scripting 2

--- Assets/_Scripts/UI/MainMenu/Mainmenu.cs
+++ Assets/_Scripts/UI/MainMenu/Mainmenu.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Level_0");
+    }
 
+    public void GameQuit()
+    {
+
+    }
 }
