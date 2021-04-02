b0b296d8751abc69ff45a0bf1e5afb122bf38521
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-04-02 10:47:25 +0700

    LevelLoader progress 3

--- Assets/_Scripts/GameManager/LevelLoader.cs
+++ Assets/_Scripts/GameManager/LevelLoader.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
@@ -18,4 +19,16 @@ public class LevelLoader : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+
 }
