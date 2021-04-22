5784a0bbadc8b40823549b57d83d714a8dc0f290
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-04-23 04:06:16 +0700

    ScreenShake progress 1

---
+++ Assets/_Scripts/CameraShake.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    Vector3 cameraInitialPosition;
+    public float shakeMag = 0.05f, shakeTime = 0.5f;
+    public Camera MainCamera;
+
+    public void Shaking()
+    {
+
+    }
+
+}
