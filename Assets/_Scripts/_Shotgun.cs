7078bbcce9ab2394ac6d797ed254ecfd50db1e4c
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-04-23 11:45:41 +0700

    Soundeffect progress 4

--- Assets/_Scripts/_Shotgun.cs
+++ Assets/_Scripts/_Shotgun.cs
@@ -43,6 +43,7 @@ public class _Shotgun : MonoBehaviour
                     Instantiate(Blast, shotPoint.position, transform.rotation);
                     timeBtwShots = StartTimeBtwShots;
 
+                    SoundManager.playSound();
                 }
             }
             else
