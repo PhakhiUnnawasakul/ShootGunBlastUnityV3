a7f262522d61f7d255467b8c9e13e70f5404a2f0
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-04-23 11:37:54 +0700

    Soundeffect progress 2

---
+++ Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip ShootSound;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        
+    }
+}
