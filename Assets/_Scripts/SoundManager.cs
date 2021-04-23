9878975f34274d73a1fa0dab59e1733770d4edd4
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-04-23 11:40:59 +0700

    Soundeffect progress 3

--- Assets/_Scripts/SoundManager.cs
+++ Assets/_Scripts/SoundManager.cs
@@ -9,6 +9,12 @@ public class SoundManager : MonoBehaviour
 
     void Start()
     {
-        
+        ShootSound = Resources.Load<AudioClip>("ShootSound");
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void playSound()
+    {
+        audioSrc.PlayOneShot(ShootSound);
     }
 }
