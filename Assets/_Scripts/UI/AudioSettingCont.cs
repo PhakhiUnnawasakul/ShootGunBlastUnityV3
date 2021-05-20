af14814d03f57a9e06176febef733e085e48ed15
Phakhi Unnawasakul <47385079+PhakhiUnnawasakul@users.noreply.github.com>
2021-05-20 18:03:40 +0700

    Sound setting Saved

---
+++ Assets/_Scripts/UI/AudioSettingCont.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class AudioSettingCont : MonoBehaviour
+{
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectPref = "SoundEffectPref";
+    private float backgroundFloat, soundEffectFloat;
+
+    public AudioSource backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
+
+    void Awake()
+    {
+        ContinueSettings();
+        
+    }
+
+    private void ContinueSettings()
+    {
+        backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+        soundEffectFloat = PlayerPrefs.GetFloat(SoundEffectPref);
+
+        backgroundAudio.volume = backgroundFloat;
+
+        for (int i = 0; i < soundEffectsAudio.Length; i++)
+        {
+            soundEffectsAudio[i].volume = soundEffectFloat;
+        }
+    }
+
+}
